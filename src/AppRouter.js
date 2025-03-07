8319d8a61685b8b11a10d257a838570c0472db7e
patilsakshi23 <sakshipatil0803@gmail.com>
2025-03-07 15:32:30 +0530

    add new route

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import VoterPage from "./Components/VoterPage";
+import Registration from "./Components/Registration.js";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/voter" element={<VoterPage />} />
+      <Route path="/register" element={<Registration />} />
     </Routes>
   );
 };

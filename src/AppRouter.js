de334fccb4c484fb251576cb9e7c319feea61d3c
patilsakshi23 <sakshipatil0803@gmail.com>
2025-04-06 09:45:48 +0530

    AppRouter page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import VoterPage from "./Pages/VoterPage";
-import Registration from "./Pages/Registration.js";
+import VoterAuth from "./Admin/VoterAuthentication.js";
+import Registration from "./Voter/Registration.js";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import VoterDashboard from "./Voter/VoterDashboard";
+import AdminDashboard from "./Admin/AdminDashboard";
+import VoteCandidate  from "./Admin/VoteCandidate.js";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/voter" element={<VoterPage />} />
+      <Route path="/admin/voter-authentication" element={<VoterAuth />} />
       <Route path="/register" element={<Registration />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/voter-dashboard" element={<VoterDashboard />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/vote-candidate" element={<VoteCandidate />} />
+
     </Routes>
   );
 };
